e2183d8d512d1703379ae249d4d592a084e66674
Adrien <adrienmarcotte@hotmail.com>
2019-07-05 13:10:15 -0400

    Insert might be working in TreePharmacy xd

--- src/pharmacy/BinaryNode.java
+++ src/pharmacy/BinaryNode.java
@@ -1,22 +1,52 @@
 package pharmacy;
 
+import primitives.PharmacyDate;
 import primitives.PharmacyItem;
 
 public class BinaryNode {
+    public final static String PRINT_BUFFER = "|";
     private BinaryNode left, right;
     private PharmacyItem value;
 
-    public BinaryNode() {
-
-    }
-
-    public BinaryNode(BinaryNode left, BinaryNode right) {
+    public BinaryNode(PharmacyItem value, BinaryNode left, BinaryNode right) {
+        this.value = value;
         this.left = left;
         this.right = right;
     }
 
+    public PharmacyItem getValue() {
+        return value;
+    }
+
+    public BinaryNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(BinaryNode left) {
+        this.left = left;
+    }
+
+    public BinaryNode getRight() {
+        return right;
+    }
+
+    public void setRight(BinaryNode right) {
+        this.right = right;
+    }
 
     public String toString() {
-        return "";
+        return "" + value + "\n" + (left != null ? left : "") + (right != null ? right : "");
+    }
+
+    public String toString(int depth) {
+        String buffer = "";
+
+        for(int i = 0; i < depth; i++) {
+            buffer += PRINT_BUFFER;
+        }
+
+        return "" + buffer + value + "\n" +
+                (left != null ? left.toString(depth + 1) : "") +
+                (right != null ? right.toString(depth + 1) : "");
     }
 }
--- src/pharmacy/TreePharmacy.java
+++ src/pharmacy/TreePharmacy.java
@@ -1,4 +1,60 @@
 package pharmacy;
 
+import primitives.PharmacyDate;
+import primitives.PharmacyItem;
+
 public class TreePharmacy {
+    BinaryNode head;
+
+    public TreePharmacy() {
+
+    }
+
+    public void insert(PharmacyItem pharmacyItem) {
+        if(head == null) {
+            head = new BinaryNode(pharmacyItem, null, null);
+            return;
+        }
+
+        BinaryNode currentNode = head;
+
+        while(currentNode != null) {
+            int difference = pharmacyItem.compareTo(currentNode.getValue());
+
+            if (difference > 0) {
+                if (currentNode.getLeft() != null) {
+                    currentNode = currentNode.getLeft();
+                } else {
+                    currentNode.setLeft(new BinaryNode(pharmacyItem, null, null));
+                    currentNode = null;
+                }
+            } else if (difference < 0) {
+                if (currentNode.getRight() != null) {
+                    currentNode = currentNode.getRight();
+                } else {
+                    currentNode.setRight(new BinaryNode(pharmacyItem, null, null));
+                    currentNode = null;
+                }
+            } else {
+                currentNode.getValue().addQuantity(pharmacyItem.getQuantity());
+                currentNode = null;
+            }
+        }
+    }
+
+    public void remove(String medication, PharmacyDate pharmacyDate) {
+
+    }
+
+    public void removeAll(String medication) {
+        //TODO Pas nécessaire pour
+    }
+
+    public void removeAll(PharmacyDate pharmacyDate) {
+        //TODO Enlever tous les éléments ayant cette data
+    }
+
+    public void removeInferior(PharmacyDate pharmacyDate) {
+        //TODO Enlever tous les éléments ayant une data inférieure à ça
+    }
 }
